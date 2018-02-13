b3ece51c3a6b11c951beee9be2a250c865ade70a
32goqud <31180848+32goqud@users.noreply.github.com>
2018-02-13 14:55:57 +0900

    Update README.m

--- README.m
+++ README.m
@@ -1,4 +1,5 @@
 1. 라이다의 laser 데이터를 python 으로 plot 하기 위한 코드 입니다.
+
 2. 
 rostopic list # 가제보 topic list
 rostopic echo * # 토픽 보기
@@ -7,6 +8,6 @@ rosmsg show *  # 메세지 타입보기 ex input) nav_msgs/Odometry
 rosrun tf view_frames # tf 그래프 출력
 evince frames.pdf # tf 그래프 보기
 
-위의 방법으로 데이터 형태 뽑아 냅니다.
-를
+위의 방법으로 데이터 형태를 뽑아 냅니다.
+
 3. 실행 : roslaunch rospy_scan scan.launch 
